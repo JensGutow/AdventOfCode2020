a1d8025c9257078123f65ee4a44bceb84b5e3e95
Jens Gutow <jens_gutow@web.de>
2020-12-19 14:21:29 +0100

    Tag 19

--- tag_18.py
+++ tag_18.py
@@ -42,6 +42,13 @@ The symbol ^ represents the power operator.
 prios_1 = {"(":0,")":0,"+":1,"*":1}
 prios_2 = {"(":0,")":0,"+":2,"*":1}
 
+# read in the puzzle input by using regex
+# puzzle structure: 
+#   - each puzzle input row is a math. taks for example: ( 1 * 4 )
+#   - each list entry: list of tupel of (Token, value) 
+#       all Math. Operatores: Token->"OP"
+#       Digits -> "D"
+#       example: (1*4) -> [("OP", "("), ("O", 1), ("OP", "*"), ("O", "4"), ("OP", ")")]
 def read_puzzle_input(file_Name):
     p = []
     pattern = re.compile('\(|\)|\*|/|\+|\d+')
@@ -59,48 +66,49 @@ def read_puzzle_input(file_Name):
             p.append(task)
     return p
 
+# return a OP b (OP is in "*" or "+")
 def calc(a, b, op):
-    if op == "+" : 
-        #("ADD", a, b)
-        return a+b
-    else: 
-        #print("MULT", a, b)
-        return a*b
+    if op == "+": return a+b
+    else        : return a*b
 
+# solution for task 1 and 2 
+# a prios dikctionary defined the operator priority : higher value <-> lower prority
 def löse(p, prios):
     summe = 0
     value = 0
     for tokens in p:
-        OPS = []
-        NRS = []
-        #print(tokens)
+        OPS = []    # stack for oparators: + * ( ) 
+        NRS = []    # stack for numbers
         for token in tokens:
-            #print("T",token, " OPS:", OPS," NRS", NRS)
+            # most simples rule: if a digit detect -> add to the number stack
             if token[0] == "D":
                 NRS.append(token[1])
             else:
                 op_new = token[1]
                 if op_new in "*+":
-                    while OPS  and prios[OPS[-1]] >= prios[op_new]:
-                        #print("calc")
+                    # if a math. operator "received" 
+                    # -> all previous operators (with "their" numbers paren) are processed.
+                    # -> but only if the "worker op" has a higher priority value as the last received operatore
+                    # (remember: higher priority value <-> lower math. priority)
+                    # !!! indirect requirement: the priority of the brackets shall higher as the priority other OPs 
+                    #   else error: calc will get the bracket.operator as input!
+                    while OPS and prios[OPS[-1]] >= prios[op_new]:
                         NRS.append(calc(NRS.pop(), NRS.pop(), OPS.pop()))
                     OPS.append(op_new)
                 elif  op_new == "(":
                     OPS.append(op_new)
-                    #print("push (")
                 elif op_new == ")":
+                    # resolving a received closed bracket -> dealing the stacks until a open is bracket detected
                     while OPS and OPS[-1] != "(":
-                        #print("calc -> )")
                         NRS.append(calc(NRS.pop(), NRS.pop(), OPS.pop()))
                     OPS.pop()
                 else:
                     print("ERRR", op_new)
         while(OPS):
-            #print("calc end")
+
             NRS.append(calc(NRS.pop(), NRS.pop(), OPS.pop()))
         summe += NRS[0]
     return summe
-                
 
 p = read_puzzle_input("tag_18.txt", )
 print(löse(p, prios_1))
